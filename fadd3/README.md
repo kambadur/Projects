2a7e3c84b0630c55fc7f43fe06d3109e298165e5
kambadur <vasam2230@gmail.com>
2022-06-03 20:33:39 +0200

    Adder3

---
+++ fadd3/README.md
@@ -0,0 +1,5 @@
+#### 3bit full adder (Adder3)
+HDLBits: [Adder3](https://hdlbits.01xz.net/wiki/Adder3)  
+rtl and testbench in verilog.  
+
+![](assets/fadd3.png)
---
+++ fadd3/tb_fadd3.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+`include "fadd3.v"
+
+module tb_fadd3;
+reg [2:0] a, b;
+reg cin;
+wire [2:0] cout;
+wire [2:0] sum;
+
+//Instantiate
+fadd3 uut(
+    .a(a),
+    .b(b),
+    .cin(cin),
+    .cout(cout),
+    .sum(sum)
+);
+
+//Initialize
+initial begin
+    $dumpfile("tb_fadd3:v");
+    $dumpvars(0, tb_fadd3);
+
+    #25 a = 7;
+    b= 6;
+    cin = 0;
+    #100 $finish;
+end
+
+endmodule
