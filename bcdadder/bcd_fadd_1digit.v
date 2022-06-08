1aa0c8ba7a07bc8b0bc19dca9dccdd9b8770448a
kambadur <vasam2230@gmail.com>
2022-06-08 11:11:52 +0200

    Updated files

--- bcdadder/bcd_fadd_1digit.v
+++ bcdadder/bcd_fadd_1digit.v
@@ -1,24 +1,34 @@
+`include "../fadd/fadd.v"
+
 module bcd_fadd_1digit (
     input [3:0] a,
     input [3:0] b,
     input cin,
-    output cout,
-    output [3:0] sum
+    output reg cout,
+    output reg [3:0] sum
 );
 
 wire [3:0] sum_binAdd;
-wire cout_binAdd;
+wire [3:0] cout_binAdd;
 
-assign sum_binAdd = a^b^cin;
-assign cout_binAdd = (a&b)|(b&cin)|(a&cin);
+//Instantiate 1-bit full adder for 4 times
+genvar i;
+generate    //3 instances
+    for(i=1; i<4; i=i+1) begin: fa
+        fadd fadd_inst1(.a(a[i]), .b(b[i]), .cin(cout_binAdd[i-1]), .cout(cout_binAdd[i]), .sum(sum_binAdd[i]));
+    end
+endgenerate
+fadd fadd_inst0(.a(a[0]), .b(b[0]), .cin(cin), .cout(cout_binAdd[0]), .sum(sum_binAdd[0])); //1 instance for lsb
 
 always @(*) begin
-    if((cout_binAdd == 1) | ((~sum_binAdd[3])&(~sum_binAdd[2]) == 1) | ((~sum_binAdd[3])&(~sum_binAdd[1]) == 1))
-        sum <= sum_binAdd + 6;
-        cout <= 1;
-    else
-        sum <= sum_binAdd;
-        cout <= cout_binAdd;
+    if((cout_binAdd[3] == 1) | ((sum_binAdd[3])&(sum_binAdd[2]) == 1) | ((sum_binAdd[3])&(sum_binAdd[1]) == 1)) begin
+        sum = sum_binAdd + 6;
+        cout = 1;
+    end
+    else begin
+        sum = sum_binAdd;
+        cout = cout_binAdd;
+    end
 end
     
 endmodule
--- bcdadder/tb_bcd_fadd_1digit.v
+++ bcdadder/tb_bcd_fadd_1digit.v
@@ -1,7 +1,8 @@
 `timescale 1ns/1ps
 `include "bcd_fadd_1digit.v"
+//`include "../fadd/fadd.v"
 
-module tb_bcd_fadd_1digit.v;
+module tb_bcd_fadd_1digit;
 reg [3:0] a, b;
 reg cin;
 wire cout;
@@ -19,7 +20,7 @@ bcd_fadd_1digit uut(
 //Initialize
 initial begin
     $dumpfile("tb_bcd_fadd_1digit.vcd");
-    $dumpvars(0. tb_bcd_fadd_1digit);
+    $dumpvars(0, tb_bcd_fadd_1digit);
 
     a= 0;
     b=0;
@@ -27,5 +28,10 @@ initial begin
     #2000 $finish;
 end
 
-always #50  
+always #50  a = a + 2;
+always #50  b = b + 2;
+always #1000 cin = 1;
+always #1100 cin = 0;
+
+endmodule
 
