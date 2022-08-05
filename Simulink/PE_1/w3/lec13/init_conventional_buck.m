e2aa4398286414328803183fca6123f6ff117b3a
Sam Kambadur <vasam2230@gmail.com>
2022-08-05 13:52:47 +0200

    Updated model

---
+++ Simulink/PE_1/w3/lec13/init_conventional_buck.m
@@ -0,0 +1,35 @@
+% init file for the model synch_buck.slx
+
+%RLC parameters
+L = 1e-6;         %inductor in H
+C = 100e-6;     %capacitor in F
+R = 1;          %Load resistance in Ohm
+r_L = 10e-3;      %inductor resistance in Ohm
+r_C = 5e-3;       %capacitor esr
+i_L_init=1;     %initial iL
+v_C_init=3.6;     %initial vC
+
+%Switch parameter
+rds_on_sw1 = 4e-3;  % high side mosfet rds on
+rds_on_sw2 = 5e-3;  % low side mosfet rds on
+
+%Diode parameters
+Vd = 0.7;  %forward voltage drop diode
+r_d = 10e-3; % diode on-state resistance
+
+%Supply and gate driver
+Vin = 12;   %Input voltage
+Vref = 3.3;    %ref output voltage
+
+V_up = 10;  % ramp peak voltage
+V_b = 0;    % ramp base voltage
+Vmax=10;    % V_up and Vmax are redundant
+
+Tsw = 1e-6;   % pwm switching time period
+Ts = 10e-9;     % Model fixed-step size
+Ton = (Vref/Vin)*Tsw;
+Toff_min = Ton/10;
+
+
+
+
