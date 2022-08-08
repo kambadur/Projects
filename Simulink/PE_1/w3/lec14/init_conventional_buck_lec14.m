e48168823f0d8a0b78c1d9fc26794b4c9dd721dd
Sam Kambadur <vasam2230@gmail.com>
2022-08-08 16:40:03 +0200

    Open loop and Closed loop controllers are integrated into the simulink model
    Open loop:
    Trailing Edge control with feed forward voltage input
    Closed loop:
    Single loop VMC
    Two loop CMC with feed forward input voltage

--- Simulink/PE_1/w3/lec14/init_conventional_buck_lec14.m
+++ Simulink/PE_1/w3/lec14/init_conventional_buck_lec14.m
@@ -15,18 +15,18 @@ rds_on_sw2 = 5e-3;  % low side mosfet rds on
 
 %Diode parameters
 Vd = 0.55;  %forward voltage drop diode
-r_d = 5e-3; % diode on-state resistance
+r_d = rds_on_sw2; % diode on-state resistance
 
 %Supply and gate driver
 Vin = 12;   %Input voltage
-Vref = 1;    %ref output voltage
+Vref = 3.3;    %ref output voltage
 
 IO_max = 20;   %maximum load current
 V_up = 10;  % ramp peak voltage
 V_b = 0;    % ramp base voltage
 Vmax=10;    % V_up and Vmax are redundant
 
-Tsw = 1e-6;   % pwm switching time period
+Tsw = 2e-6;   % pwm switching time period
 Ts = 10e-9;     % Model fixed-step size
 Ton = (Vref/Vin)*Tsw;
 Toff_min = Ton/10;
@@ -34,9 +34,9 @@ Toff_min = Ton/10;
 Vff = 1/Vin;    % feed-forward voltage gain
 
 Vref_switch = 1;    % switch to turn on Vref transient change
-IL_transient_en = 0; % load current transient enable
-DCM_en = 1;         % discontinuous conduction mode enable
+I0_transient_en = 1; % load current transient enable
+DCM_en = 0;         % discontinuous conduction mode enable
 ideal_psu_en = 0;       % selection between ideal and practical voltage sources
-input_volt_trans_en = 1;    % input voltage disturbance enable (after 1.7ms input voltage dropped by 4V)
-Vff_en = 1;         % input voltage feed forward switch for duty cycle adaptation
+input_volt_trans_en = 0;    % input voltage disturbance enable (after 1.7ms input voltage dropped by 4V)
+Vff_en = 0;         % input voltage feed forward switch for duty cycle adaptation
 
