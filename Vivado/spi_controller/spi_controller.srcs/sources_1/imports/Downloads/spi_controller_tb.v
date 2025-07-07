3097562ec785fd1c375d8b2ec91ccfd2b6d8bd85
Sameera Kumar Kambadur <vasam2230@gmail.com>
2025-07-07 09:05:05 +0200

    updated testbench

--- Vivado/spi_controller/spi_controller.srcs/sources_1/imports/Downloads/spi_controller_tb.v
+++ Vivado/spi_controller/spi_controller.srcs/sources_1/imports/Downloads/spi_controller_tb.v
@@ -1,46 +1,47 @@
-`include "spi_controller.v"
+//`include "spi_controller_100.v"
+`include "spi_controller_1.v"
 `timescale 1ns / 1ps 
 
-module spi_controller_tb();
+module spi_controller_100_tb();
 	reg clk;					// FPGA clock
-	reg reset;				// Reset line
-	//reg [7:0] control;	// 8-bit control byte
-	//reg [7:0] data;		// 8-bit daty byte
+	reg nreset;				// Reset line
+	reg [7:0] control;	// 8-bit control byte
+	reg [7:0] data;		// 8-bit data byte
 	reg start;				// start command
+	wire sclk;			// derived SPI clock
 	wire nCS;				// chipselect
 	wire mosi;			// serial MOSI
 	wire done;
-	wire sclk;
 
-	
-	//Instantitae the UUT
+	//Instantiate the UUT
 	spi_controller uut0(
 		.clk(clk),
-		.reset(reset),
-		//.control(control),
-		//.data(data),
+		.nreset(nreset),
+		.control(control),
+		.data(data),
 		.start(start),
+		.sclk(sclk),
 		.nCS(nCS),
 		.mosi(mosi),
-		.done(done),
-		.sclk(sclk)
+		.done(done)
 	);
 	
 	initial begin
 		clk = 0;
-		reset = 1;
-		//control = 8'hFF;
-		//data = 8'hAB;
-		//start = 1;
+		nreset = 0;
+		control = 8'h00;
+		data = 8'h7F;
+		start = 0;
 	end
 	
-	always #50 clk = !clk;	//100MHz FPGA clock
+	always #5 clk = !clk;	//100MHz FPGA clock
 	
 	initial begin
-		#200 	reset = 1'b0;
-		//#200		start = 1'b0;
-		//#200		start = 1'b1;
-		#5000	$finish;
+		#50 	nreset = 1'b0;
+		#60 	nreset = 1'b1;
+		#20		start = 1'b1;
+		#200		start = 1'b0;
+		#20000	$finish;
 	end
 	
 	initial begin
@@ -51,11 +52,7 @@ module spi_controller_tb();
 	/*
 	initial begin
 		$display("\t, \ttime, \tclk, \treset, \tstart, \tmosi, \tdone");
-		$monitor("%d, \t%d, \t%d, \t%d, \%d, \%d", $time, clk, reset, start, mosi, done);
+		$monitor("%d, \t%d, \t%d, \t%d, \%d, \%d", $time, clk, nreset, start, mosi, done);
 	end
 	*/
 endmodule
-	
-	
-	
-	
