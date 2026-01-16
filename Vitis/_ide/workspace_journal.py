440a20098c440e37281bea33b2e7ea4c066def42
Sameera Kumar Kambadur <vasam2230@gmail.com>
2026-01-16 09:54:18 +0100

    Vitis repo from t440p

---
+++ Vitis/_ide/workspace_journal.py
@@ -0,0 +1,8 @@
+# 2025-01-14T14:00:06.912158
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="Vitis")
+
+vitis.dispose()
+
---
+++ Vitis/_ide/workspace_journal_1679752.py
@@ -0,0 +1,8 @@
+# 2025-01-02T17:24:39.844944
+import vitis
+
+client = vitis.create_client()
+client.set_workspace(path="Vitis")
+
+vitis.dispose()
+
